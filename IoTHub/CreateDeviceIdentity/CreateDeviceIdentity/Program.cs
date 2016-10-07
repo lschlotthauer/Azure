648fad4d888e4305dae6176de44e969a7a091745
lschlotthauer <lucas6sic6@gmail.com>
2016-10-07 11:07:59 -0300

    Device Identity

---
+++ IoTHub/CreateDeviceIdentity/CreateDeviceIdentity/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Azure.Devices;
+using Microsoft.Azure.Devices.Common.Exceptions;
+
+namespace CreateDeviceIdentity
+{
+    class Program
+    {
+        static RegistryManager registryManager;
+        static string connectionString = "HostName=IoTHubUno.azure-devices.net;SharedAccessKeyName=iothubowner;SharedAccessKey=vsxxQV2rz/zh027yORVhM37k5/CXUV/PUFnu6Q1TKVY=";
+
+        static void Main(string[] args)
+        {
+            registryManager = RegistryManager.CreateFromConnectionString(connectionString);
+            AddDeviceAsync().Wait();
+            Console.ReadLine();
+        }
+
+        private static async Task AddDeviceAsync()
+        {
+            string deviceId = "myFirstDevice";
+            Device device;
+            try
+            {
+                device = await registryManager.AddDeviceAsync(new Device(deviceId));
+            }
+            catch (DeviceAlreadyExistsException)
+            {
+                device = await registryManager.GetDeviceAsync(deviceId);
+            }
+            Console.WriteLine($"Generated device key {device.Authentication.SymmetricKey.PrimaryKey}");
+        }
+    }
+}
